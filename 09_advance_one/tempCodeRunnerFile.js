9b779e6ebadf81f43aa755ecafc688d8a55eaa67
uditrajput03 <87136581+uditrajput03@users.noreply.github.com>
2023-12-26 12:03:49 +0000

    ..

--- 09_advance_one/tempCodeRunnerFile.js
+++ 09_advance_one/tempCodeRunnerFile.js
@@ -1,4 +1,1 @@
-leo.then(() => {
-//     console.log("Resolve Executed");
-
-// }).catch(() => (console.log("reject executed"))).finally(() => console.log("Promises Completed"))
+hiteshchoudhary
---
+++ 10_classes_and_oop/test.js
@@ -0,0 +1,9 @@
+function leo(){
+    let a = "Let";
+    var b = "var"
+    const c = "const"
+    d = "Notype"
+}
+leo.e = "defined"
+leo();
+console.log(leo.e);
